f8495685c58ea262742adce256182ba33aff8908
ALBERT <ishengomaalbert@gmail.com>
2023-11-17 14:49:44 +0300

    Lower delay time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ async function getRecipes() {
   const recipes = JSON.parse(file);
 
   //delay response
-  await new Promise((resolve) => setTimeout(resolve, 3000))
+  await new Promise((resolve) => setTimeout(resolve, 1000))
 
   return recipes;
 
